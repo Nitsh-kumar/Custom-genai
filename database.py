68e9b320e5ab6a5f05bff9347bad77906fc38bba
Sauravvvvv <66158077+Saurav1704@users.noreply.github.com>
2024-08-07 14:37:59 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,35 +1,9 @@
 import sqlite3
 import os
 from sql import set_db
-# import requests
-# from requests.auth import HTTPBasicAuth 
-
-# def get_data_from_service(service):
-#     odata_username = "Jchand" 
-#     odata_password = "Bakzee@123"    
-#     try:
-#         auth = HTTPBasicAuth(odata_username, odata_password)
-#         response = requests.get(service , auth = auth)
-#         response.raise_for_status()
-#         service_data = response.json()
-#         # st.write("Raw OData response:", data)  # Debugging line
- 
-#         if 'd' in service_data and 'results' in service_data['d']:
-#             return_data = service_data['d']['results']
-#             return return_data, None
-#         else:
-#             return None, "No BOM data found."
-#     except requests.exceptions.RequestException as e:
-#         return None, str(e)
-
-
-
-
-
 
 def read_sql_query(st,sql):
     db = 'MYdb.db'
-    
     if not os.path.isfile(db):
         set_db()
 
@@ -49,7 +23,7 @@ def read_sql_query(st,sql):
        cur.execute(sql)
        rows = cur.fetchall()
     except Exception as e:
-           st.write(e)
+           st.write(str(e))
     column_names = []
     for description in cur.description:
         if desc.get(description[0]):
